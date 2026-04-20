96e3e3fb7bf25b8270fd326a5823c499a8acbeba
davidContrerasCorp <david.contreras-d@mail.escuelaing.edu.co>
2026-04-19 20:03:42 -0500

    feat:clase necesaria para ejecutar la aplicacion en eclipse

--- src/stackingitems/Main.java
+++ src/stackingitems/Main.java
@@ -1,5 +1,5 @@
 package stackingitems;
-
+//clase para ejecutar el simulador(por requerimientos de eclipse se debe crear)
 public class Main {
     public static void main(String[] args) {
         Tower t = new Tower(5);   // crea torre con 5 tazas
